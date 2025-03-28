a1b4354d9ca083dc879554cfc9a379dca6a844d1
Lucas Klein <lucas.klein@atak.com.br>
2025-03-28 16:05:13 -0300

    Correção do EnvLoader para suportar as duas tabelas utilizadas

--- env_loader.py
+++ env_loader.py
@@ -5,7 +5,9 @@ def load_env():
     load_dotenv()
     config = {
         "NOTION_TOKEN": os.getenv("NOTION_TOKEN"),
-        "NOTION_DATABASE_ID": os.getenv("NOTION_DATABASE_ID"),
+        "NOTION_REWARDS_DB_ID": os.getenv("NOTION_REWARDS_DB_ID"),
+        "NOTION_MISSIONS_DB_ID": os.getenv("NOTION_MISSIONS_DB_ID"),
         "NOTION_VERSION": os.getenv("NOTION_VERSION", "2022-06-28")
     }
+
     return config
